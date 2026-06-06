07ab93c22ba8e0dfeb7035218de73252acc4947b
PranavThorat1432 <pranavthorat95@gmail.com>
2026-06-06 17:24:08 +0530

    Error Middleware

---
+++ server/src/middleware/error.middleware.js
@@ -0,0 +1,43 @@
+const errorHandler = (err, req, res, next) => {
+  console.error("Express Error Handler caught:", err);
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message || "Internal Server Error";
+
+  // Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    statusCode = 400;
+    message = "Duplicate field value entered";
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+  }
+
+  // JWT errors
+  if (err.name === "JsonWebTokenError") {
+    statusCode = 401;
+    message = "Invalid token";
+  }
+
+  if (err.name === "TokenExpiredError") {
+    statusCode = 401;
+    message = "Token expired";
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export default errorHandler;
