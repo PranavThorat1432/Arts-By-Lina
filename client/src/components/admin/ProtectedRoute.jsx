44f7e24c68574efcdaf95b3e029f2b57cdcc9dbb
PranavThorat1432 <pranavthorat95@gmail.com>
2026-08-18 19:34:39 +0530

    Routes

---
+++ client/src/components/admin/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import Loader from "../common/Loader";
+
+const ProtectedRoute = ({ children }) => {
+  const { admin, loading } = useAuth();
+
+  if (loading) return <Loader fullScreen />;
+  if (!admin) return <Navigate to="/admin/login" replace />;
+
+  return children;
+};
+
+export default ProtectedRoute;
